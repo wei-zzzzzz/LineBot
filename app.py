c7252e951fda632b507010159fe515d3e70993bd
wei-zzzzzz <71373961+wei-zzzzzz@users.noreply.github.com>
2024-05-21 22:15:13 +0800

    Update and rename lambda_function.py to app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+import json
+from choose import make_bubble 
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import  *
+
+line_bot_api = LineBotApi(os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
+
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST','GET'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        print(body, signature)
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+    
+# @handler.add(JoinEvent)
+# def handle_join(event):
+#     newcoming_text = "查詢上市臺股資訊問我就對了！！"
+#     line_bot_api.reply_message(event.reply_token, TextMessage(text=newcoming_text))
+
+# 處理訊息
+@handler.add(MessageEvent)
+def handle_message(event):
+    # event->使用者資料
+    
+    message = TextSendMessage(text='Hi')
+    line_bot_api.reply_message(event.reply_token, message)
+
+    return 0
