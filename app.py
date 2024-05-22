ee0c53e670746f44cfa1990faaa9adfae96773e9
wei-zzzzzz <71373961+wei-zzzzzz@users.noreply.github.com>
2024-05-22 21:45:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,7 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, message)
 
     return 0
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
