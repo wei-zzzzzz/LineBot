e6d570e1721f0f52517169e3afc5d33b42e118a7
wei-zzzzzz <71373961+wei-zzzzzz@users.noreply.github.com>
2024-05-22 22:23:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def handle_message(event):
     return 0
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='54.254.162.138', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
