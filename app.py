e0bb1be908ef26c275ea3a1a17690668280b945d
willy <abc9450208@gmail.com>
2024-06-26 22:05:34 +0800

    test

--- app.py
+++ app.py
@@ -15,17 +15,14 @@ handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 
 @app.route("/<username>")
 def sendMessage(username):
-  try:
+  
     df = get_info(username)
     message = f'早安先生/女士，您的小孩已到校～'
     print(str(df["parents"][0]))
     line_bot_api.push_message("U5a24e475af75ef9f17e6c12877b10539", TextSendMessage(text=message))
     # line_bot_api.push_message(str(df["userID"][0]), TextSendMessage(text=message)) {str(df["parents"][0])} {str(df["student"][0])}
 
-
-    return 'OK'
-  except:
-    print('error')
+    return 'get OK'
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST','GET'])
