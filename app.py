f21fe392df1a4bec2ee769e922d8bf49b93a036f
willy <abc9450208@gmail.com>
2024-07-01 22:43:37 +0800

    update_20240701

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 @app.route("/send/<username>", methods=['GET'])
 def sendMessage(username):
     df = get_info(username)
-    message = f'早安{str(df["parents"][0])}先生/女士，您的小孩{str(df["student"][0])}已到校～'
+    message = f'早安先生/女士，您的小孩已到校～'
     
-    # line_bot_api.push_message("U5a24e475af75ef9f17e6c12877b10539", TextSendMessage(text=message))
-    line_bot_api.push_message(str(df["userID"][0]), TextSendMessage(text=message))
+    line_bot_api.push_message("U5a24e475af75ef9f17e6c12877b10539", TextSendMessage(text=message))
+    # line_bot_api.push_message(str(df["userID"][0]), TextSendMessage(text=message))
 
     return 'get OK'
 
